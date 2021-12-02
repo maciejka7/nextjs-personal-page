00caf7f30d0d04af58b1422a0e91ecc2d822158f
Michał Maciejewski <maciejewskimichal7@gmail.com>
2021-12-02 13:57:11 +0100

    refactor / relocate file / add header component

---
+++ src/components/Header/types.ts
@@ -0,0 +1,7 @@
+export interface HeaderProps {
+    type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+    size?: 'small' | 'medium' | 'large'
+    children?: React.ReactNode
+    gradient? : boolean,
+    text: string
+}
